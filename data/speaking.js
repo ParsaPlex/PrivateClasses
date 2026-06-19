0abc1965c4d5cfeb1e3af3b3ac885724a2b6a263
ParsaPlex <parsagolmohamadi77@gmail.com>
2026-06-19 17:55:21 +0330

    Create speaking.js

---
+++ data/speaking.js
@@ -0,0 +1,23 @@
+const speakingCueCards=[
+
+{
+title:"Describe a place you like to study",
+points:[
+"Where it is",
+"When you go there",
+"Why you like it",
+"How it helps you study"
+]
+},
+
+{
+title:"Describe a teacher who helped you",
+points:[
+"Who the teacher was",
+"What they taught",
+"How they helped you",
+"Why you remember them"
+]
+}
+
+]
