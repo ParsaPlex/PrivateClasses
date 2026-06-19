cb37e8c7731970eaa0878511b0816e554a502715
ParsaPlex <parsagolmohamadi77@gmail.com>
2026-06-19 17:54:45 +0330

    Create writing.js

---
+++ data/writing.js
@@ -0,0 +1,18 @@
+const writingTopics=[
+
+{
+title:"Request Training",
+prompt:"Write a letter to your manager asking for additional language training."
+},
+
+{
+title:"Restaurant Complaint",
+prompt:"Write a letter to the restaurant manager complaining about the food."
+},
+
+{
+title:"Lost Coat",
+prompt:"Write to your friend about the coat you left at their house."
+}
+
+]
